28e9286bf2154231b140133eadd1e1e4d02e47fc
ammar <ammarbinfaisal@bitbucket.org>
2018-05-28 20:08:34 +0530

    restructuring the project

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 #!/usr/bin/env node
 
-const inquirer = require("./lib/inquirer");
-const clear = require("clear");
+const inquirer = require("./lib/inquirer"),
+	{ setDefault } = require("./lib/setDefault"),
+	{ useDefault } = require("./lib/useDefault"),
+	pkg = require("./lib/pkg"),
+	log = require("./lib/logger"),
+	fs = require("fs"),
+	meow = require("meow");
 
-let settings = [];
+const cli = meow(
+	`
+	
+	Options
+		--set-default, -D  set the the current directory's prettier config file as default
+		--use-default, -d  use the default config file in the current directory
+`,
+	{
+		flags: {
+			"set-default": {
+				type: "boolean",
+				alias: "D"
+			},
+			"use-default": {
+				type: "boolean",
+				alias: "d"
+			}
+		}
+	}
+);
 
-async function ask() {
-	settings = await inquirer.inquire();
+if (cli.flags.setDefault) setDefault();
+else if (cli.flags.useDefault) useDefault();
+else {
+	// check for package.json file
+	// if it exists then prompt the questions to the user
+	if (!pkg.exists()) {
+		log.warning("There is no package.json file in the current directory.");
+		inquirer.confirmCreationOfConfigFile();
+	} else if (!pkg.hasPrettierAsDevDependency()) {
+		log.warning("\x1b[1mprettier is not a dev dependency of this project"); // \x1b[0m === reset & \x1b[1m === bright
+		inquirer.confirmCreationOfConfigFile();
+	} else inquirer.askSetupQuestions();
 }
-ask();
