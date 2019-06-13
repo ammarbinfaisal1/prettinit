53718f82fe9a8ce67112588e3266912de52e427d
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-06-13 08:33:05 +0530

    keep printWidth as integer - fixes #4

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ const cli = meow(
 	}
 );
 
-let flagUsed = false;
+let flagNotUsed = true;
 for (const flag in cli.flags) {
 	if (Object.prototype.hasOwnProperty.call(cli.flags, "flag") && cli.flags[flag]) {
-		flagUsed = true;
+		flagNotUsed = false;
 		break;
 	}
 }
@@ -40,17 +40,17 @@ if (cli.flags.setDefault) {
 	setDefault();
 } else if (cli.flags.writeDefault) {
 	writeDefault();
-} else if (!flagUsed) {
+} else if (flagNotUsed) {
 	// Check for package.json file
 	// if it exists then prompt the questions to the user
 	if (!pkg.exists()) {
 		log.warn("There is no package.json file in the current directory.");
 		inquirer.confirmCreationOfConfigFile();
-	} else if (!pkg.hasPrettierAsDevDependency()) {
+	} else if (pkg.hasPrettierAsDevDependency()) {
+		inquirer.askSetupQuestions();
+	} else {
 		log.warn("prettier is not a dev dependency of this project");
 		inquirer.confirmCreationOfConfigFile();
-	} else {
-		inquirer.askSetupQuestions();
 	}
 } else {
 	log.error("An invalid flag has been used.");
